5401c95de6486782d342990ff1fb2fab37dcd8a7
craig-novawm <craig@novawm.com>
2025-04-15 14:13:11 +0100

    Update to TaskOrchestrator to show task guidance
    Updates advice orechestrator flow, but any place that uses the task orchestrator will need to be udpated

--- force-app/main/default/lwc/taskOrchestrator/taskOrchestrator.js
+++ force-app/main/default/lwc/taskOrchestrator/taskOrchestrator.js
@@ -1,31 +1,100 @@
 import { LightningElement, api } from 'lwc';
-//import { RefreshEvent } from 'lightning/refresh';
+import { NavigationMixin } from 'lightning/navigation';
+import getTaskInfo from '@salesforce/apex/LwcHelperTaskOrchestrator.getTaskInfoByTaskName';
 
-export default class TaskOrchestrator extends LightningElement {
+export default class TaskOrchestrator extends NavigationMixin(LightningElement) {
 
-    @api recordId;
-    @api objectApiName;
-    @api cardTitle = "Manage Task"
-    @api cardIcon = "standard:task";
-    @api flowApiName;
-    
 
-    get inputVariables() {
-        return [
-          {
-            name: "recordId",
-            type: "String",
-            value: this.recordId
-          }
-        ];
+  @api recordId;
+  @api objectApiName;
+  @api cardTitle = "Manage Task"
+  @api cardIcon = "standard:task";
+  @api flowApiName;
+
+  showTaskSelectionFlow = true;
+  showOrchestatorFlow = false;
+  showHelpButton = false;
+  
+
+  get taskSelectionInputVariables() {
+    return [
+      {
+        name: "recordId",
+        type: "String",
+        value: this.recordId
       }
+    ];
+  }
 
-    handleStatusChange(event) {
-        if(event.detail.status === 'FINISHED') {
-            //Refresh just the record view and the LWC
-            //this.dispatchEvent(new RefreshEvent());
-            //Reload the entire page
-            window.location.reload();
-        }
+  get orchestrationInputVariables(){
+    return [
+      {
+        name: "recordId",
+        type: "String",
+        value: this.recordId
+      },
+      {
+        name: "task",
+        type: "SObject",
+        value: this.selectedTask
+      }
+    ];
+  }
+
+  _selectedTask
+  @api
+  get selectedTask(){
+    return this._selectedTask;
+  }
+  set selectedTask(value){
+    this._selectedTask = value;
+    this.getTaskInfo();
+  }
+  
+  @api taskInformation;
+
+  async getTaskInfo(){
+    console.log('getting task info');
+    if(this.selectedTask.OW_task_information_identifier__c){
+
+      this.taskInformation = await getTaskInfo({taskTypeName: this.selectedTask.OW_task_information_name__c});
+
+      if(this.taskInformation && this.taskInformation.OW_task_description_google_doc_link__c){
+        this.showHelpButton = true;
+      }
     }
+  }
+
+  handleHelpClick() {
+    window.open(this.taskInformation.OW_task_description_google_doc_link__c, '_blank');
+  }
+
+  handleTaskSelectionStatusChange(event){
+
+    if(event.detail.status === 'FINISHED_SCREEN') {
+      const outputVariables = event.detail.outputVariables;
+
+      for (let i = 0; i < outputVariables.length; i++) {
+        const outputVar = outputVariables[i];
+
+        if (outputVar.name === "selectedTask") {
+          this.selectedTask = outputVar.value;
+
+          if(this.selectedTask !== null){
+            this.showTaskSelectionFlow = false;
+            this.showOrchestatorFlow = true;
+          }
+        }
+      }
+    }
+  }  
+
+  handleOrchestrationStatusChange(event) {
+    if(event.detail.status === 'FINISHED') {
+        //Refresh just the record view and the LWC
+        //this.dispatchEvent(new RefreshEvent());
+        //Reload the entire page
+        window.location.reload();
+    }
+  }
 }
