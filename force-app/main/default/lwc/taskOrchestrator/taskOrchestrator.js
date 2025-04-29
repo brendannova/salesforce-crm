316b7c3eee6f2fe96e30fdfb16b9cce374f47297
Cian Morrissey <cian@novawm.com>
2025-04-29 15:34:27 +0000

    Reverting changes to original task orchestrator LWC.

--- force-app/main/default/lwc/taskOrchestrator/taskOrchestrator.js
+++ force-app/main/default/lwc/taskOrchestrator/taskOrchestrator.js
@@ -1,99 +1,31 @@
 import { LightningElement, api } from 'lwc';
-import getTaskInfo from '@salesforce/apex/LwcHelperTaskOrchestrator.getTaskInfoByTaskName';
+//import { RefreshEvent } from 'lightning/refresh';
 
 export default class TaskOrchestrator extends LightningElement {
 
+    @api recordId;
+    @api objectApiName;
+    @api cardTitle = "Manage Task"
+    @api cardIcon = "standard:task";
+    @api flowApiName;
+    
 
-  @api recordId;
-  @api objectApiName;
-  @api cardTitle = "Manage Task"
-  @api cardIcon = "standard:task";
-  @api flowApiName;
-
-  showTaskSelectionFlow = true;
-  showOrchestatorFlow = false;
-  showHelpButton = false;
-  
-
-  get taskSelectionInputVariables() {
-    return [
-      {
-        name: "recordId",
-        type: "String",
-        value: this.recordId
-      }
-    ];
-  }
-
-  get orchestrationInputVariables(){
-    return [
-      {
-        name: "recordId",
-        type: "String",
-        value: this.recordId
-      },
-      {
-        name: "task",
-        type: "SObject",
-        value: this.selectedTask
-      }
-    ];
-  }
-
-  _selectedTask
-  @api
-  get selectedTask(){
-    return this._selectedTask;
-  }
-  set selectedTask(value){
-    this._selectedTask = value;
-    this.getTaskInfo();
-  }
-  
-  @api taskInformation;
-
-  async getTaskInfo(){
-    console.log('getting task info');
-    if(this.selectedTask.OW_task_information_identifier__c){
-
-      this.taskInformation = await getTaskInfo({taskTypeName: this.selectedTask.OW_task_information_name__c});
-
-      if(this.taskInformation && this.taskInformation.OW_task_description_google_doc_link__c){
-        this.showHelpButton = true;
-      }
-    }
-  }
-
-  handleHelpClick() {
-    window.open(this.taskInformation.OW_task_description_google_doc_link__c, '_blank');
-  }
-
-  handleTaskSelectionStatusChange(event){
-
-    if(event.detail.status === 'FINISHED_SCREEN') {
-      const outputVariables = event.detail.outputVariables;
-
-      for (let i = 0; i < outputVariables.length; i++) {
-        const outputVar = outputVariables[i];
-
-        if (outputVar.name === "selectedTask") {
-          this.selectedTask = outputVar.value;
-
-          if(this.selectedTask !== null){
-            this.showTaskSelectionFlow = false;
-            this.showOrchestatorFlow = true;
+    get inputVariables() {
+        return [
+          {
+            name: "recordId",
+            type: "String",
+            value: this.recordId
           }
-        }
+        ];
       }
-    }
-  }  
 
-  handleOrchestrationStatusChange(event) {
-    if(event.detail.status === 'FINISHED') {
-        //Refresh just the record view and the LWC
-        //this.dispatchEvent(new RefreshEvent());
-        //Reload the entire page
-        window.location.reload();
+    handleStatusChange(event) {
+        if(event.detail.status === 'FINISHED') {
+            //Refresh just the record view and the LWC
+            //this.dispatchEvent(new RefreshEvent());
+            //Reload the entire page
+            window.location.reload();
+        }
     }
-  }
 }
